546483803eb6c8768f1be69d260da5bcc7916dcb
kchang312 <kchang312@gatech.edu>
2025-06-03 10:48:36 -0400

    Update Header.tsx

--- src/app/[locale]/components/Header.tsx
+++ src/app/[locale]/components/Header.tsx
@@ -1,40 +1,58 @@
 'use client'
 import { Link } from '@/src/navigation'
 import { useTranslations } from 'next-intl'
-import { FC } from 'react'
+import { FC, useState, useEffect } from 'react'
 import GithubIcon from '../../icons/github'
 import LogoIcon from '../../icons/logo'
 import LangSwitcher from './LangSwitcher'
 import ThemeSwitch from './ThemeSwitch'
+
 interface Props {
   locale: string
 }
+
 export const Header: FC<Props> = ({ locale }) => {
   const t = useTranslations('')
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 0)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <header className='sticky top-0 left-0 w-full bg-white shadow z-50'>
-    <div className='mx-auto flex max-w-screen-2xl flex-row items-center justify-between p-5'>
-      <Link lang="en" href='/'>
-        <div className='flex flex-row items-center'>
-          <div className='mb-2 h-14 w-14'>
-            <LogoIcon />
+    <header
+      className={`sticky top-0 left-0 w-full z-50 transition-colors duration-300 ${
+        scrolled ? 'bg-white shadow' : 'bg-transparent'
+      }`}
+    >
+      <div className='mx-auto flex max-w-screen-2xl flex-row items-center justify-between p-5'>
+        <Link lang='en' href='/'>
+          <div className='flex flex-row items-center'>
+            <div className='mb-2 h-14 w-14'>
+              <LogoIcon />
+            </div>
+            <strong className='mx-2 select-none'>Template</strong>
           </div>
-          <strong className='mx-2 select-none'>Template</strong>
+        </Link>
+        <div className='flex flex-row items-center gap-3'>
+          <nav className='mr-10 inline-flex gap-5'>
+            <Link lang={locale} href={`/about`}>
+              {t('About')}
+            </Link>
+            <a href=''>{t('Experience')}</a>
+            <a href=''>{t('Projects')}</a>
+            <a href=''>{t('Resume')}</a>
+          </nav>
+          <ThemeSwitch />
+          <LangSwitcher />
         </div>
-      </Link>
-      <div className='flex flex-row items-center gap-3'>
-        <nav className='mr-10 inline-flex gap-5'>
-          <Link lang={locale} href={`/about`}>
-            {t('About')}
-          </Link>
-          <a href=''>{t('Experience')}</a>
-          <a href=''>{t('Projects')}</a>
-          <a href=''>{t('Resume')}</a>
-        </nav>
-        <ThemeSwitch />
-        <LangSwitcher />
       </div>
-    </div>
     </header>
   )
 }
+
