582d80227b9f6ed9cebde9cc7918d1efa2267b2f
kchang312 <kchang312@gatech.edu>
2025-06-05 12:48:16 -0400

    Update Footer.tsx

--- src/app/[locale]/components/Footer.tsx
+++ src/app/[locale]/components/Footer.tsx
@@ -1,3 +1,10 @@
 export const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className="w-full bg-[rgb(105,117,101)] text-center text-sm text-white py-6 mt-10">
+      <div className="space-y-2">
+        <p>Contact: kc6107j@gmail.com</p>
+        <p>Website Created by Katherine Chang © 2025</p>
+      </div>
+    </footer>
+  )
 }
